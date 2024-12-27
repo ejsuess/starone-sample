c6867c25c3023f5bba0c1d9a9ae4599bc790fe24
Liz Gilbert <egilbert@imediainc.com>
2024-12-27 17:09:28 -0500

    footer development

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -17,4 +17,13 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+  const footerBlock = document.querySelector('footer');
+  const starReviews = footerBlock.querySelector('.information');
+  if (starReviews) {
+    const informationSection = starReviews.querySelector('.default-content-wrapper');
+    if (informationSection) {
+      informationSection.insertAdjacentHTML('beforeend', '<div id="pr-reviewsnippet"></div><script type="text/javascript" src="//ui.powerreviews.com/stable/4.0/ui.js"></script><script>POWERREVIEWS.display.render({api_key: "51e40959-7c8c-44f2-a879-8bb40f26029b",locale: "en_US",merchant_group_id: "78934",merchant_id: "934181",page_id: "membership",review_wrapper_url:"https://www.starone.org/reviews/write.php?pr_page_id=membership", style_sheet: "https://www.starone.org/css/pr.css", on_read_reviews_click: read_reviews, product:{ name: "Star One Membership", url: "https://www.starone.org/membership/refer-a-friend/", image_url: "https://www.starone.org/images/star-one-logo.png", description: "As a financial cooperative, Star One exists solely to serve members, and our profits are returned to you in many ways.", category_name: "Membership", }, components: { ReviewSnippet: "pr-reviewsnippet" } }); </script>');
+    }
+  }
 }
